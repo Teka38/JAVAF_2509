b09676568489527ce8881b6cd76f79e12cd2aa2c
TEKA <yoniteka10@gmail.com>
2026-07-13 21:00:39 +0300

    Add all changes and update repository

---
+++ src/com/adse2509/sess03_selection_and_iteration/TextSearch.java
@@ -0,0 +1,61 @@
+package sess03_selection_and_iteration;
+
+/**
+ * Java program to search for some text in a paragraph using a while loop.
+ *
+ * @author a.nyanjui
+ */
+public class TextSearch
+{
+
+    // main method begins program execution
+    public static void main(String[] args)
+    {
+        // paragraph to search text within
+        String paragraph = "Themes and styles also help keep your document coordinated."
+                + " When you click Design and choose a new Theme,"
+                + " the pictures, charts, and SmartArt graphics change to match your new theme."
+                + " When you apply styles, your headings change to match the new theme."
+                + " Save time in Word with new buttons that show up where you need them."
+                + " To change the way a picture fits in your document,"
+                + " click it and a button for layout options appears next to it."
+                + " When you work on a table, click where you want to add a row or a column,"
+                + " and then click the plus sign. Reading is easier, too, in the new Reading view. "
+                + "You can collapse parts of the document and focus on the text you want."
+                + " If you need to stop reading before you reach the end, Word remembers where you left off"
+                + " - even on another device."
+                + " Video provides a powerful way to help you prove your point."
+                + " When you click Online Video, you can paste in the embed code for the video you want to add."
+                + " You can also type a keyword to search online for the video that best fits your document";
+        // Word/text to search for
+        String textToSearch = "ADSE2509";
+        boolean found = false; // Variable to indicate whether the text has been       found or not
+        int index = 0;
+
+        // Use a while loop to search for the text
+        while (index < paragraph.length())
+        {
+            // Check if the substring matches the text to search
+            if (index + textToSearch.length() <= paragraph.length()
+                    && paragraph.substring(index, index + textToSearch.length()).
+                            equals(textToSearch))
+            {
+                found = true;
+                break; // Exit the loop when the text is found
+            }
+            index++; // Increment the index to check the next character
+        }
+
+        // Output the results
+        if (found)
+        {
+            System.out.println("The text/word '" + textToSearch + "' was found "
+                    + "at index " + index);
+        } else
+        {
+            System.out.println("Unfortunately the text/word '" + textToSearch
+                    + "' was not found in the paragraph.");
+        }
+    }
+
+}
---
+++ src/com/adse2509/sess04_classes_objects_and_methods/MethodOverloadingDemo.java
@@ -0,0 +1,39 @@
+package sess04_classes_objects_and_methods;
+
+/**
+ * Java program to demonstrate how to use overloaded methods.
+ *
+ * @author Teka
+ */
+public class MethodOverloadingDemo
+{
+
+    // main method begins program execution
+    public static void main(String[] args)
+    {
+        // Variables to be used in the program
+        int firstNum = 3, secondNum = 5, thirdNum = 7, fourthNum = 8;
+        byte firstByte = 4;
+        long answer;
+
+        MethodOverloading ml = new MethodOverloading();
+        // Add two integers and display their sum
+        answer = ml.addNums(firstNum, secondNum);
+        System.out.println("The sum of " + firstNum + " and " + secondNum + " "
+                + "is " + answer);
+
+        // Add three integers and display their sum
+        answer = ml.addNums(firstNum, secondNum, thirdNum);
+        System.out.println("The sum of " + firstNum + ", " + secondNum + " and "
+                + thirdNum + " is " + answer);
+
+        // Add a byte & an integer and display their sum
+        answer = ml.addNums(firstByte, firstNum);
+        System.out.println("The sum of " + firstByte + " and " + firstNum + " "
+                + "is " + answer);
+
+        // Add several integers and display their sum
+        answer = ml.addNums(firstNum, secondNum, thirdNum, fourthNum, 22, 14, 180);
+        System.out.println("The sum of the various integers is " + answer);
+    }
+}
